0685b4ed9cfa824324f362dfa8dbb1cd4a702ca5
yahir2108 <jesus.aceves5039@alumnos.udg.mx>
2026-04-29 13:10:01 -0600

    Fix middleware export for vercel

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware(req) {
+    // El middleware se encarga de proteger las rutas
+  }
+);
 
 export const config = {
   matcher: [
     "/",
     "/clientes/:path*",
     "/cotizaciones/:path*",
+    "/cobranza/:path*",
   ],
 };
